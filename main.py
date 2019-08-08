2fc24c84e51136bec3f50f24a4e2cdb6d24f056f
Ashlee Kupor <aekupor@gmail.com>
2019-08-07 21:13:14 -0700

    added class object and created 2 instances

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
 )
 
+class Course:
+    def __init__(self, title, units, defaultQ):
+        self.title = title
+        self.units = units
+        self.defaultQ = defaultQ
+
+CS106A = Course("CS106A", 5, "1A")
+CS106B = Course("CS106B", 5, "1W")
+
 class Home(webapp2.RequestHandler):
     def get(self):
-        welcome = "hello"
         template_vars = {
-            "welcome": welcome,
+            
         }
         template = jinja_env.get_template('templates/index.html')
         self.response.write(template.render(template_vars))
