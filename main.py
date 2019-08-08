d71f0938a98589c86053b4d0b1116f536eb84304
Ashlee Kupor <aekupor@gmail.com>
2019-08-07 22:02:26 -0700

    added allCourses list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import jinja2
 import webapp2
+import jinja2
 import os
 
 jinja_env = jinja2.Environment(
@@ -7,18 +7,23 @@ jinja_env = jinja2.Environment(
 )
 
 class Course:
-    def __init__(self, title, units, defaultQ):
+    def __init__(self, title, units, defaultQ, subject):
         self.title = title
         self.units = units
         self.defaultQ = defaultQ
+        self.subject = subject
 
-CS106A = Course("CS106A", 5, "1A")
-CS106B = Course("CS106B", 5, "1W")
+allCourses = []
+
+CS106A = Course("CS106A", 5, "1A", "Engr")
+allCourses.append(CS106A)
+CS106B = Course("CS106B", 5, "1W", "Engr")
+allCourses.append(CS106B)
 
 class Home(webapp2.RequestHandler):
     def get(self):
         template_vars = {
-            
+            "allCourses": allCourses,
         }
         template = jinja_env.get_template('templates/index.html')
         self.response.write(template.render(template_vars))
