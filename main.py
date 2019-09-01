5fa344d8b88a24f1923c4857f20fb086c4271f07
Ashlee Kupor <aekupor@gmail.com>
2019-08-31 20:14:08 -0700

    created code to ensure that all pre-reqs are made both for courses before and after the course that was moved

--- main.py
+++ main.py
@@ -9,23 +9,24 @@ jinja_env = jinja2.Environment(
 
 class Course:
     # TODO: add courses that have that course as a prereq so can check when moving
-    def __init__(self, title, units, defaultQ, subject, description, prereqs):
+    def __init__(self, title, units, defaultQ, subject, description, prereqs, needed):
         self.title = title
         self.units = units
         self.defaultQ = defaultQ
         self.subject = subject
         self.description = description
         self.prereqs = prereqs
+        self.needed = needed
 
 allCourses = []
 
-CS106A = Course("CS106A", 5, "1A", "Engr", "intro to programming", "none")
+CS106A = Course("CS106A", 5, "1A", "Engr", "intro to programming", "none", "CS106B")
 allCourses.append(CS106A)
-think = Course("Think", 5, "1A", "Other", "required class", "none")
+think = Course("Think", 5, "1A", "Other", "required class", "none", "none")
 allCourses.append(think)
-CS106B = Course("CS106B", 5, "1W", "Engr", "intro to programming part 2", "CS106A")
+CS106B = Course("CS106B", 5, "1W", "Engr", "intro to programming part 2", "CS106A", "CS107")
 allCourses.append(CS106B)
-CS107 = Course("CS107", 5, "1S", "Engr", "intro to programming part 3", "CS106A CS106B")
+CS107 = Course("CS107", 5, "1S", "Engr", "intro to programming part 3", "CS106A CS106B", "none")
 allCourses.append(CS107)
 
 class Home(webapp2.RequestHandler):
--- scripts/index.js
+++ scripts/index.js
@@ -12,30 +12,55 @@ let column = "";
 let course = "";
 let previous = "";
 let prereqs = "";
+let neededFor = "";
 let temp = "";
+let temp1 = "";
 let start="";
+let start1="";
 let end="";
+let end1="";
 let previousLength = "";
 let unstripped = "";
 let courseName = "";
 let coursesBefore = [];
+let coursesAfter = [];
 let prereqsMade = false;
+let neededMade = false;
 
 function drop(ev) {
   ev.preventDefault();
   data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
-  column = document.getElementById(ev.srcElement.id).children;
   temp = document.getElementById(data).getElementsByClassName('button');
   prereqs = temp[0].innerHTML.toString();
-  start = prereqs.indexOf("Pre-reqs: ")
-  end = prereqs.indexOf("data-original-title")
-  temp = prereqs.substring(start+10, end-2);
+  start = prereqs.indexOf("Pre-reqs: ");
+  start1 = prereqs.indexOf("Needed for: ");
+  end1 = prereqs.indexOf("data-original-title");
+  end = prereqs.indexOf(". Needed");
+  temp = prereqs.substring(start+10, end);
+  temp1 = prereqs.substring(start1+12, end1-2);
   prereqs = temp.split(" ");
+  neededFor = temp1.split(" ");
   course = document.getElementById(data).id;
-  for (counter = 0; counter < columnOrder.length; counter++){
-    if (columnOrder[counter] == ev.target.id){
-      break;
+
+  for (let i = 0; i < columnOrder.length; i++) {
+    if (columnOrder[i] == ev.target.id){
+      column = i;
+    }
+  }
+
+  // creates list of coursesBefore and coursesAfter the moved course
+  for (counter = 0; counter < columnOrder.length; counter++) {
+    if (counter == column){
+      continue;
+    }
+    else if (counter > column){
+      previous = document.getElementById(columnOrder[counter]);
+      previousLength = previous.getElementsByTagName('p').length;
+      for (count = 0; count < previousLength; count++){
+        unstripped = previous.getElementsByTagName('p')[count];
+        coursesAfter.push(unstripped.innerHTML)
+      }
     }
     else {
       previous = document.getElementById(columnOrder[counter]);
@@ -46,6 +71,8 @@ function drop(ev) {
       }
     }
   }
+
+  // checks if the prereqs of course are met
   if ((prereqs == "none") == false) {
     for (counter = 0; counter < prereqs.length; counter++) {
       prereqsMade = false;
@@ -56,9 +83,26 @@ function drop(ev) {
         }
       }
       if (prereqsMade == false){
-        alert("the pre-reqs of this class have not been fulfilled. please move it back");
+        alert("The pre-reqs of this class have not been fulfilled. Please move it back.");
+      }
+    }
+  }
+
+  // checks to make sure that other classes previous in schedule do not need this course as a pre-req
+  if ((neededFor == "none") == false) {
+    for (counter = 0; counter < neededFor.length; counter++) {
+      neededMade = false;
+      for (counter1 = 0; counter1 < coursesAfter.length; counter1++){
+        if (neededFor[counter] == coursesAfter[counter1]) {
+          neededMade = true;
+          break;
+        }
+      }
+      if (neededMade == false){
+        alert("This class is a pre-req for another course. Please move it back.");
       }
     }
   }
   coursesBefore=[];
+  coursesAfter=[];
 }
